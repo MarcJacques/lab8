68602846bd6777159290180910c6edee6a5fe3a0
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-29 23:03:05 +0000

    add while loop

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+// lab8.cpp - Use a while loop to print messages declaring your passion for
+
 #include <iostream>
 
+using namespace std;
+
 int main() {
-  std::cout << "Hello World!\n";
+
+  int n;
+
+  cout << "How many times should I print?";
+  cin >> n;
+  
+  int i = 1;
+  while (i <= n) {
+    cout << i << " " << "I love Computer Science!!"<< endl;
+    i++;
+  }
+  cout << "Printed this message " << n << " times.";
+  
+  
+  return 0;
 }
